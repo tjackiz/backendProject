876a650dccdef574d21617abf2f8e997df436b06
tjackiz <bhokenmaier@web.de>
2023-03-04 15:53:10 +0100

    [TASK] added jpa annotations and validations to actor entity

--- entertainmentService/src/main/java/de/tjackiz/entertainmentService/model/Actor.java
+++ entertainmentService/src/main/java/de/tjackiz/entertainmentService/model/Actor.java
@@ -2,20 +2,35 @@ package de.tjackiz.entertainmentService.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import de.tjackiz.entertainmentService.model.common.AbstractBase;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
 
 import java.util.Date;
 import java.util.Objects;
 
+@Entity
+@Table(name = "TABLE_ACTOR")
 public class Actor extends AbstractBase {
 
+    @NotBlank
     private String forename;
 
+    @NotBlank
     private String surname;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
+    @Temporal(TemporalType.DATE)
+    @Past
     private Date birthdate;
 
-    // 0-10
+    @Min(0)
+    @Max(10)
     private int imdbRating;
 
     public String getForename() {
