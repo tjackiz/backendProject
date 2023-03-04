78246dace997eb78a76d26eebbedacc7a4c39f91
tjackiz <bhokenmaier@web.de>
2023-03-04 11:23:25 +0100

    [TASK] added MovieController implementation

--- gatewayService/src/main/java/de/tjackiz/gatewayService/controller/entertainmentService/MovieController.java
+++ gatewayService/src/main/java/de/tjackiz/gatewayService/controller/entertainmentService/MovieController.java
@@ -1,4 +1,55 @@
 package de.tjackiz.gatewayService.controller.entertainmentService;
 
+import de.tjackiz.gatewayService.model.entertainmentService.movie.AbstractMovie;
+import de.tjackiz.gatewayService.service.entertainmentService.MovieService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/movies")
 public class MovieController {
+
+    private final MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AbstractMovie>> getMovies() {
+        List<AbstractMovie> movieList = movieService.getMovieList();
+        return movieList == null || movieList.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(movieList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AbstractMovie> getMovieById(@PathVariable UUID id) {
+        AbstractMovie movie = movieService.getMovieById(id);
+        return movie == null ? ResponseEntity.noContent().build() : ResponseEntity.ok(movie);
+    }
+
+    @PostMapping
+    public ResponseEntity<UUID> createMovie(@RequestBody AbstractMovie movie) {
+        UUID id = movieService.createMovie(movie);
+        return id == null ? ResponseEntity.noContent().build() : new ResponseEntity(id, HttpStatus.CREATED);
+    }
+
+    // TODO what return type should be used here ?
+    @PutMapping("/{id}")
+    public ResponseEntity<AbstractMovie> updateMovie(@Valid @PathVariable UUID id, @RequestBody AbstractMovie movie) {
+        movie = movieService.updateMovie(id, movie);
+        return movie == null ? ResponseEntity.noContent().build() : ResponseEntity.ok(movie);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteMovie(@Valid @PathVariable UUID id) {
+        movieService.deleteMovie(id);
+        return ResponseEntity.noContent().build();
+    }
 }
