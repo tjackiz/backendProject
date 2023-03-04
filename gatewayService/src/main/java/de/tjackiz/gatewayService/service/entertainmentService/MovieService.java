e126b1b8773844146273bcb5ad8288b3235722ff
tjackiz <bhokenmaier@web.de>
2023-03-04 11:23:00 +0100

    [TASK] added first implementation of movieService

--- gatewayService/src/main/java/de/tjackiz/gatewayService/service/entertainmentService/MovieService.java
+++ gatewayService/src/main/java/de/tjackiz/gatewayService/service/entertainmentService/MovieService.java
@@ -1,4 +1,22 @@
 package de.tjackiz.gatewayService.service.entertainmentService;
 
+import de.tjackiz.gatewayService.model.entertainmentService.movie.AbstractMovie;
+import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+import java.util.UUID;
+
+@Validated
 public interface MovieService {
+
+    AbstractMovie getMovieById(@Valid UUID id);
+
+    UUID createMovie(@Valid AbstractMovie movie);
+
+    AbstractMovie updateMovie(@Valid UUID id, @Valid AbstractMovie movie);
+
+    List<AbstractMovie> getMovieList();
+
+    void deleteMovie(@Valid UUID id);
 }
--- gatewayService/src/main/java/de/tjackiz/gatewayService/service/entertainmentService/impl/MovieServiceImpl.java
+++ gatewayService/src/main/java/de/tjackiz/gatewayService/service/entertainmentService/impl/MovieServiceImpl.java
@@ -1,9 +1,68 @@
 package de.tjackiz.gatewayService.service.entertainmentService.impl;
 
+import de.tjackiz.gatewayService.model.entertainmentService.movie.AbstractMovie;
+import de.tjackiz.gatewayService.service.entertainmentService.MovieService;
+import de.tjackiz.gatewayService.service.webClientService.WebClientService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j
-public class MovieServiceImpl {
+public class MovieServiceImpl implements MovieService {
+
+    private final WebClientService webClientService;
+
+    @Autowired
+    public MovieServiceImpl(WebClientService webClientService) {
+        this.webClientService = webClientService;
+    }
+
+    @Override
+    public AbstractMovie getMovieById(UUID id) {
+        String uri = "http://localhost:8082/actors/" + id.toString();
+        Mono<AbstractMovie> mono = webClientService.get(uri, new ParameterizedTypeReference<AbstractMovie>() {
+        });
+
+        return mono.block();
+    }
+
+    @Override
+    public UUID createMovie(AbstractMovie movie) {
+        String uri = "http://localhost:8082/actors";
+        Mono<UUID> mono = webClientService.post(uri, movie, new ParameterizedTypeReference<UUID>() {
+        });
+
+        return mono.block();
+    }
+
+    @Override
+    public AbstractMovie updateMovie(UUID id, AbstractMovie movie) {
+        String uri = "http://localhost:8082/actors/" + id.toString();
+        Mono<AbstractMovie> mono = webClientService.put(uri, movie, new ParameterizedTypeReference<AbstractMovie>() {
+        });
+
+        return mono.block();
+    }
+
+    // TODO replace with pagination
+    @Override
+    public List<AbstractMovie> getMovieList() {
+        String uri = "http://localhost:8082/actors";
+        Mono<List<AbstractMovie>> mono = webClientService.get(uri, new ParameterizedTypeReference<List<AbstractMovie>>() {
+        });
+
+        return mono.block();
+    }
+
+    @Override
+    public void deleteMovie(UUID id) {
+        String uri = "http://localhost:8082/actors/" + id.toString();
+        webClientService.delete(uri);
+    }
 }
