d301cf9088b6ac4be126bccba0ed91e8735f2523
tjackiz <bhokenmaier@web.de>
2023-03-16 21:25:58 +0100

    [TASK] added GetActorSet and DeleteActor

--- entertainmentService/src/main/java/de/tjackiz/entertainmentService/controller/ActorController.java
+++ entertainmentService/src/main/java/de/tjackiz/entertainmentService/controller/ActorController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Set;
 import java.util.UUID;
 
 @RestController
@@ -26,6 +27,12 @@ public class ActorController {
         return actor == null ? ResponseEntity.noContent().build() : ResponseEntity.ok(actor);
     }
 
+    @GetMapping
+    public ResponseEntity<Set<Actor>> getActors() {
+        Set<Actor> actorSet = actorService.getActorSet();
+        return actorSet == null || actorSet.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(actorSet);
+    }
+
     @PostMapping
     public ResponseEntity<UUID> createActor(@RequestBody Actor actor) {
         UUID id = actorService.createActor(actor);
@@ -39,5 +46,11 @@ public class ActorController {
         // TODO service could load object from db and overwrite all fields or at least id property with new value und write to db
         // TODO OR assign id and write directly to db, avoid reading request
         return ResponseEntity.ok(UUID.randomUUID());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteActor(@PathVariable UUID id) {
+        actorService.deleteActor(id);
+        return ResponseEntity.noContent().build();
     }
 }
