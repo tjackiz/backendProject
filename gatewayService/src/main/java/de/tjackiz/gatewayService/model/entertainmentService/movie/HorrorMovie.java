f1b97e9649a7af8d2614d395312d75c46db86ec1
tjackiz <bhokenmaier@web.de>
2023-03-16 21:28:27 +0100

    [TASK] added HorrorMovie

---
+++ gatewayService/src/main/java/de/tjackiz/gatewayService/model/entertainmentService/movie/HorrorMovie.java
@@ -0,0 +1,40 @@
+package de.tjackiz.gatewayService.model.entertainmentService.movie;
+
+import jakarta.validation.constraints.Min;
+
+import java.util.Objects;
+
+public class HorrorMovie extends AbstractMovie {
+
+    @Min(0)
+    private int avgScreamDecibel;
+
+    public int getAvgScreamDecibel() {
+        return avgScreamDecibel;
+    }
+
+    public void setAvgScreamDecibel(int avgScreamDecibel) {
+        this.avgScreamDecibel = avgScreamDecibel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        HorrorMovie that = (HorrorMovie) o;
+        return avgScreamDecibel == that.avgScreamDecibel;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), avgScreamDecibel);
+    }
+
+    @Override
+    public String toString() {
+        return "HorrorMovie{" +
+                "avgScreamDecibel=" + avgScreamDecibel +
+                '}';
+    }
+}
